063896162cbb10c28bf1109cfc24194f46afec93
Gerald Meier <garyee@gmx.de>
2016-10-07 15:51:25 +0200

    Update modules.php

--- languages/de/modules.php
+++ languages/de/modules.php
@@ -8,7 +8,7 @@
  * @package   facebook-photo-gallery 
  * @author    Mario Gienapp
  * @license   MIT License
- * @copyright POSTYOU Werbeagentur 2016
+ * @copyright POSTYOU 2016
  */
  
 $GLOBALS['TL_LANG']['MOD']['facebook_photo_gallery'][0] = 'Facebook Fotoalben';
